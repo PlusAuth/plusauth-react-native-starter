46b29140690954349292ecbc5bd5b69c0d1a3c6b
Harun Özceyhan <harunozceyhan@gmail.com>
2022-02-25 16:20:07 +0300

    chore: add android project

--- plusauth-env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  issuer: 'https://starters.plusauth.com',
-  clientId: 'dezwrNoatwzUJO4FtsOFIWyDQbTMex3dfXlOaGQrCHPCcRFp',
-  redirectUrl: 'com.plusauth.reactnativeexample:/oauthredirect/login',
-  postLogoutRedirectUrl: 'com.plusauth.reactnativeexample:/',
-  scopes: ['openid', 'profile', 'email', 'offline_access']
-}
